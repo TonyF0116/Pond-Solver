47efe1c6707b73e1915638297ecd64419d34321d
Yuang Fan <yuangf2@illinois.edu>
2023-05-20 05:01:07 +0800

    First function version

---
+++ Pond_Solver.py
@@ -0,0 +1,206 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QComboBox, QLabel, QInputDialog
+from PyQt5.QtGui import QPixmap
+from solver import solver
+
+class Pond_Solver(QMainWindow):
+    def __init__(self, manuel_input = []):
+        super().__init__()
+        
+        self.blocks = []
+        self.blocks.append(['Empty'])
+        self.label_list = []
+        self.label_list.append(['Empty'])
+
+        self.setWindowTitle("Pond_Solver")
+        self.setGeometry(100, 100, 850, 1000)
+
+        canvas = QPixmap("res/canvas.png")
+        self.red = QPixmap("res/red.png")
+        self.yellow_horizontal = QPixmap("res/yellow_horizontal.png")
+        self.yellow_vertical = QPixmap("res/yellow_vertical.png")
+        self.blue_horizontal = QPixmap("res/blue_horizontal.png")
+        self.blue_vertical = QPixmap("res/blue_vertical.png")
+        
+        canvas_label = QLabel(self)
+        canvas_label.setPixmap(canvas)
+        canvas_label.setGeometry(100, 100, 650, 600)
+
+        self.row_label = QLabel('Row index:', self)
+        self.row_label.setGeometry(50, 750, 100, 50)
+        self.row_idx = QLineEdit(self)
+        self.row_idx.setGeometry(50, 800, 100, 50)
+
+        self.col_label = QLabel('Column index:', self)
+        self.col_label.setGeometry(250, 750, 120, 50)
+        self.col_idx = QLineEdit(self)
+        self.col_idx.setGeometry(250, 800, 100, 50)
+
+        self.orientation_label = QLabel('Orientation index:', self)
+        self.orientation_label.setGeometry(450, 750, 120, 50)
+        self.orientation = QComboBox(self)
+        self.orientation.addItem('Horizontal')
+        self.orientation.addItem('Vertical')
+        self.orientation.setGeometry(450, 800, 100, 50)
+
+        self.color_label = QLabel('Color index:', self)
+        self.color_label.setGeometry(650, 750, 120, 50)
+        self.color = QComboBox(self)
+        self.color.addItem('Red')
+        self.color.addItem('Yellow')
+        self.color.addItem('Blue')
+        self.color.setGeometry(650, 800, 100, 50)
+
+        self.add_block_button = QPushButton('Add block', self)
+        self.add_block_button.setGeometry(100, 900, 100, 50)
+        self.add_block_button.clicked.connect(self.add_block)
+
+        self.remove_last_button = QPushButton('Remove last', self)
+        self.remove_last_button.setGeometry(250, 900, 150, 50)
+        self.remove_last_button.clicked.connect(self.remove_last)
+
+        self.solve_button = QPushButton('Solve', self)
+        self.solve_button.setGeometry(450, 900, 100, 50)
+        self.solve_button.clicked.connect(self.solve)
+
+        if manuel_input != []:
+            for i in range(1, len(manuel)):
+                self.manuel_add_block(manuel_input[i])
+
+
+    def manuel_add_block(self, block):
+        self.blocks.append(block)
+        # print(self.blocks)
+
+        if block[1] == 'Red':
+            label = QLabel(self)
+            label.setPixmap(self.red)
+            label.setGeometry(100 * (block[4] + 1), 100 * (block[3] + 1), 200, 100)
+            label.show()
+        elif block[1] == 'Yellow':
+            if block[2] == 'Horizontal':
+                label = QLabel(self)
+                label.setPixmap(self.yellow_horizontal)
+                label.setGeometry(100 * (block[4] + 1), 100 * (block[3] + 1), 200, 100)
+                label.show()
+            elif block[2] == 'Vertical':
+                label = QLabel(self)
+                label.setPixmap(self.yellow_vertical)
+                label.setGeometry(100 * (block[4] + 1), 100 * (block[3] + 1), 100, 200)
+                label.show()
+        elif block[1] == 'Blue':
+            if block[2] == 'Horizontal':
+                label = QLabel(self)
+                label.setPixmap(self.blue_horizontal)
+                label.setGeometry(100 * (block[4] + 1), 100 * (block[3] + 1), 300, 100)
+                label.show()
+            elif block[2] == 'Vertical':
+                label = QLabel(self)
+                label.setPixmap(self.blue_vertical)
+                label.setGeometry(100 * (block[4] + 1), 100 * (block[3] + 1), 100, 300)
+                label.show()
+        self.label_list.append(label)
+
+
+    def add_block(self):
+        color = self.color.currentText()
+        orientation = self.orientation.currentText()
+        x1 = int(self.row_idx.text())
+        y1 = int(self.col_idx.text())
+        x2 = x1
+        y2 = y1
+        if color == 'Red':
+            y2 += 1
+        elif color == 'Yellow':
+            if orientation == 'Horizontal':
+                y2 += 1
+            else:
+                x2 += 1
+        elif color == 'Blue':
+            if orientation == 'Horizontal':
+                y2 += 2
+            else:
+                x2 += 2
+        block = [len(self.blocks), color, orientation, x1, y1, x2, y2]
+        self.manuel_add_block(block)
+
+    def remove_last(self):
+        # print(self.blocks)
+        if len(self.blocks) == 1:
+            return
+        self.blocks.pop(len(self.blocks)-1)
+        self.label_list[len(self.label_list)-1].deleteLater()
+        self.label_list.pop(len(self.label_list)-1)
+
+    def solve(self):
+        text, ok = QInputDialog.getText(self, '', 'Sure you want to start the solving process?')
+        if not ok:
+            return
+        # print(self.blocks)
+        self.solution = solver(self.blocks)
+        
+        # print(self.solution)
+        if len(self.solution) == 1:
+            print('Unsolvable')
+            print(self.blocks)
+            exit()
+        
+        self.current_index = 0
+
+        self.row_label.setVisible(False)
+        self.row_idx.setVisible(False)
+        self.col_label.setVisible(False)
+        self.col_idx.setVisible(False)
+        self.orientation_label.setVisible(False)
+        self.orientation.setVisible(False)
+        self.color_label.setVisible(False)
+        self.color.setVisible(False)
+        self.add_block_button.setVisible(False)
+        self.remove_last_button.setVisible(False)
+        self.solve_button.setVisible(False)
+
+
+        self.title = QLabel("Current step: {}, Total step: {}".format(self.current_index+1, len(self.solution)), self)
+        self.title.setGeometry(100,50,300,50)
+        self.title.show()
+
+
+        self.button_prev = QPushButton("Previous", self)
+        self.button_prev.clicked.connect(self.show_previos_dict)
+        self.button_prev.move(300, 725)
+        self.button_prev.show()
+        self.button_next = QPushButton("Next", self)
+        self.button_next.clicked.connect(self.show_next_dict)
+        self.button_next.move(400, 725)
+        self.button_next.show()
+
+    def show_previos_dict(self):
+        # self.label.setVisible(False)
+        self.current_index = (self.current_index - 1) % len(self.solution)
+        self.title.setText("Current step: {}, Total step: {}".format(self.current_index+1, len(self.solution)))
+        
+        current_step = self.solution[self.current_index]
+        # print(current_step)
+
+        for i in range(1, len(current_step)):
+            block = current_step[i]
+            self.label_list[block[0]].move(100 * (block[4] + 1), 100 * (block[3] + 1))
+
+    def show_next_dict(self):
+        self.current_index = (self.current_index + 1) % len(self.solution)
+        self.title.setText("Current step: {}, Total step: {}".format(self.current_index+1, len(self.solution)))
+        
+        current_step = self.solution[self.current_index]
+        # print(current_step)
+
+        for i in range(1, len(current_step)):
+            block = current_step[i]
+            # print(block)
+            self.label_list[block[0]].move(100 * (block[4] + 1), 100 * (block[3] + 1))
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    manuel = [['Empty'], [1, 'Red', 'Horizontal', 2, 0, 2, 1], [2, 'Yellow', 'Horizontal', 0, 0, 0, 1], [3, 'Yellow', 'Horizontal', 4, 2, 4, 3], [4, 'Yellow', 'Horizontal', 5, 2, 5, 3], [5, 'Yellow', 'Horizontal', 5, 4, 5, 5], [6, 'Yellow', 'Horizontal', 3, 4, 3, 5], [7, 'Yellow', 'Vertical', 0, 3, 1, 3], [8, 'Yellow', 'Vertical', 0, 5, 1, 5], [9, 'Yellow', 'Vertical', 2, 3, 3, 3], [10, 'Yellow', 'Vertical', 4, 1, 5, 1], [11, 'Blue', 'Vertical', 3, 0, 5, 0], [12, 'Blue', 'Vertical', 0, 4, 2, 4]]
+    window = Pond_Solver()
+    window.show()
+    sys.exit(app.exec())
---
+++ solver.py
@@ -0,0 +1,233 @@
+def get_char_indices(string, character):
+    indices = []
+    for i in range(len(string)):
+        if string[i] == character:
+            indices.append(i)
+    return indices
+
+def blocks_to_map(blocks):
+    result = ['00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00']
+    for i in range(1, len(blocks)):
+        block = blocks[i]
+        if block[2] == 'Horizontal':
+            for i in range(block[4], block[6]+1):
+                if block[0] < 10:
+                    result[block[3]*6+i] = '0' + str(block[0])
+                else:
+                    result[block[3]*6+i] = str(block[0])
+        elif block[2] == 'Vertical':
+            for i in range(block[3], block[5]+1):
+                if block[0] < 10:
+                    result[6*i+block[4]] = '0' + str(block[0])
+                else:
+                    result[6*i+block[4]] = str(block[0])
+        else:
+            print('Invalid orientation')
+            exit()
+
+    return ''.join(result)
+
+def map_to_blocks(original_map):
+    map = []
+    for i in range(0, len(original_map), 2):
+        map.append(original_map[i:i+2])
+    result = []
+    result.append(['Empty'])
+    for i in range(1, 19):
+        block = [0, 'Color', 'Orientation', -1, -1, -1, -1]
+        if i < 10:
+            indices = get_char_indices(map, '0'+str(i))
+        else:
+            indices = get_char_indices(map, str(i))
+        if len(indices) == 0:
+            break
+        elif len(indices) == 2:
+            if i == 1:
+                block[0] = 1
+                block[1] = 'Red'
+                block[2] = 'Horizontal'
+                block[3] = 2
+                block[4] = indices[0] - 12
+                block[5] = 2
+                block[6] = indices[1] - 12
+            else:
+                if indices[1]-indices[0] == 1:
+                    block[0] = i
+                    block[1] = 'Yellow'
+                    block[2] = 'Horizontal'
+                    block[3] = int(indices[0] / 6)
+                    block[4] = indices[0] - 6 * block[3]
+                    block[5] = block[3]
+                    block[6] = indices[1] - 6 * block[3]
+                else:
+                    block[0] = i
+                    block[1] = 'Yellow'
+                    block[2] = 'Vertical'
+                    block[3] = int(indices[0] / 6)
+                    block[4] = indices[0] % 6
+                    block[5] = int(indices[1] / 6)
+                    block[6] = indices[1] % 6
+        elif len(indices) == 3:
+            if indices[1]-indices[0] == 1:
+                block[0] = i
+                block[1] = 'Blue'
+                block[2] = 'Horizontal'
+                block[3] = int(indices[0] / 6)
+                block[4] = indices[0] - 6 * block[3]
+                block[5] = block[3]
+                block[6] = indices[2] - 6 * block[3]
+            else:
+                block[0] = i
+                block[1] = 'Blue'
+                block[2] = 'Vertical'
+                block[3] = int(indices[0] / 6)
+                block[4] = indices[0] % 6
+                block[5] = int(indices[2] / 6)
+                block[6] = indices[2] % 6
+        else:
+            print('Invalid block length')
+            exit()
+
+        result.append(block)
+    return result
+
+# Takes in the current blocks, return the map format of all possible moves
+def get_possible_moves(blocks):
+    result = []
+    original_map = blocks_to_map(blocks)
+    map = []
+    for i in range(0, len(original_map), 2):
+        map.append(original_map[i:i+2])
+
+    for i in range(1, len(blocks)):
+        block = blocks[i]
+        # print(block)
+        if block[2] == 'Horizontal':
+            start = block[3] * 6 + block[4]
+            end = block[5] * 6 + block[6]
+            if block[4] != 0 and map[start-1] == '00':
+                tmp = list(map)
+                if block[0] < 10:
+                    tmp[start-1] = '0' + str(block[0])
+                else:
+                    tmp[start-1] = str(block[0])
+                tmp[end] = '00'
+                result.append(''.join(tmp))
+            if block[6] != 5 and map[end+1] == '00':
+                tmp = list(map)
+                tmp[start] = '00'
+                if block[0] < 10:
+                    tmp[end+1] = '0' + str(block[0])
+                else:
+                    tmp[end+1] = str(block[0])
+                result.append(''.join(tmp))
+
+        elif block[2] == 'Vertical':
+            start = block[3] * 6 + block[4]
+            end = block[5] * 6 + block[6]
+            if block[3] != 0 and map[start-6] == '00':
+                tmp = list(map)
+                if block[0] < 10:
+                    tmp[start-6] = '0' + str(block[0])
+                else:
+                    tmp[start-6] = str(block[0])
+                tmp[end] = '00'
+                result.append(''.join(tmp))
+            if block[5] != 5 and map[end+6] == '00':
+                tmp = list(map)
+                tmp[start] = '00'
+                if block[0] < 10:
+                    tmp[end+6] = '0' + str(block[0])
+                else:
+                    tmp[end+6] = str(block[0])
+                result.append(''.join(tmp))
+    
+    return result
+
+
+def solved(blocks):
+    original_map = blocks_to_map(blocks)
+    map = []
+    for i in range(0, len(original_map), 2):
+        map.append(original_map[i:i+2])
+    for i in range(17, 12, -1):
+        if map[i] == '00':
+            continue
+        elif map[i] == '01':
+            return True
+        else:
+            break
+    return False
+
+def print_map(map):
+    txt = ''
+    for i in range(6):
+        for j in range(0, 12, 2):
+            txt += map[12*i+j:12*i+j+2] + ' '
+        txt += '\n'
+    print(txt)
+
+# The graph for the solver is tree like
+# Each blocks have n children, where n refers to the sum of all possible moves from the current blocks
+# Use BFS to find the solution with the least moves, then use backtracking to get the solution
+def solver(blocks):
+    first_map = blocks_to_map(blocks)
+    visited = set() # Set of visited maps
+    added = set()
+    added.add(first_map)
+    to_visit = [] # List of maps to visit
+    backtracking = {} # blocks of children/parent relation between maps
+    to_visit.append(first_map)
+
+    last_visited = first_map
+
+    while len(to_visit) != 0:
+        # print(len(to_visit))
+        cur_map = to_visit[0]
+        to_visit.pop(0)
+        if cur_map in visited:
+            continue
+        # print_map(cur_map)
+        cur_blocks = map_to_blocks(cur_map)
+        
+        visited.add(cur_map)
+
+        if solved(cur_blocks):
+            last_visited = cur_map
+            break
+
+        possible_moves = get_possible_moves(cur_blocks)
+        for move in possible_moves:
+            if move in visited or move in added:
+                continue
+            else:
+                # print_map(move)
+                to_visit.append(move)
+                added.add(move)
+                backtracking[move] = cur_map
+    
+    result = [] # Result in map format
+    result.append(last_visited)
+    index = 0
+    while backtracking.get(result[index]) != None:
+        result.append(backtracking[result[index]])
+        index += 1
+    result.reverse()
+    result_blocks = []
+    for element in result:
+        result_blocks.append(map_to_blocks(element))
+    return result_blocks
+
+
+if __name__ == "__main__":
+    # Map format: Row by row, separated by space between each row
+    # sample_map = '220356 000356 110450 800477 890000 890000'
+    # blocks starting from 1, where 1 is always the red block, 0 reserved for blanks
+    sample_blocks = [['Empty'], [1, 'Red', 'Horizontal', 2, 0, 2, 1], [2, 'Yellow', 'Horizontal', 0, 0, 0, 1], [3, 'Yellow', 'Horizontal', 4, 2, 4, 3], [4, 'Yellow', 'Horizontal', 5, 2, 5, 3], [5, 'Yellow', 'Horizontal', 5, 4, 5, 5], [6, 'Yellow', 'Horizontal', 3, 4, 3, 5], [7, 'Yellow', 'Vertical', 0, 3, 1, 3], [8, 'Yellow', 'Vertical', 0, 5, 1, 5], [9, 'Yellow', 'Vertical', 2, 3, 3, 3], [10, 'Yellow', 'Vertical', 4, 1, 5, 1], [11, 'Blue', 'Vertical', 3, 0, 5, 0], [12, 'Blue', 'Vertical', 0, 4, 2, 4]]
+    # Format: Index, Color, Orientation, x1, y1, x2, y2, ordering in x1, y1 smaller than x2, y2
+    # sample_blocks[1] = ['1', 'Red', 'Horizontal', 2, 0, 2, 1]
+    # print(blocks_to_map(sample_blocks))
+    # print(solved(map_to_blocks(sample_map)))
+    # sample_blocks = map_to_blocks(sample_map)
+    print(solver(sample_blocks))
+
